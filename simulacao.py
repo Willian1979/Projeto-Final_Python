7ca400d3dd9dca06b8a56750d06e4618041e90f1
Willian1979 <68518771+Willian1979@users.noreply.github.com>
2020-08-24 19:35:59 -0300

    2ª Versão do Trabalho Final de Phyton

--- simulacao.py
+++ simulacao.py
@@ -1,4 +1,5 @@
-from classes import *
+import random
+from classes import Banco, Cliente
 
 num_clientes = 10000
 num_bancos = 10
@@ -24,7 +25,7 @@ pesos = [
 ]
 
 
-class Simulacao():
+class Simulacao:
 
     def __init__(self):
         self.clientes = list()
@@ -34,7 +35,8 @@ class Simulacao():
         for i in range(1, num_bancos + 1):
             self.bancos.append(Banco(i))
 
-        pesos_selecionados = random.choices(pesos, weights=[0.1*num_clientes, 0.2*num_clientes, 0.7*num_clientes], k=num_clientes)
+        pesos_selecionados = random.choices(pesos, weights=[0.1 * num_clientes, 0.2 * num_clientes, 0.7 * num_clientes],
+                                            k=num_clientes)
 
         for j in range(1, num_clientes + 1):
             banco_escolhido = random.choice(self.bancos)
@@ -47,7 +49,7 @@ class Simulacao():
                    'esperada,anuidade cobrada, experiencia com anuidade,qualidade de atendimento minima esperada,'
                    'qualidade de atendimento prestado,experiencia com atendimento, media de nota']
 
-        media_por_faixa = {
+        satisfacao_por_faixa = {
             9706: {
                 7: 0,
                 3: 0,
@@ -68,7 +70,7 @@ class Simulacao():
             },
         }
 
-        medias = []
+        avaliacoes = []
         num_zero = 0
         num_tres = 0
         num_sete = 0
@@ -79,31 +81,31 @@ class Simulacao():
             experiencia_anuidade = cliente.verifica_anuidade()
             experiencia_qualidade = cliente.verifica_qualidade()
 
-            media = (experiencia_anuidade * cliente.pesos['anuidade'] + experiencia_tarifa * cliente.pesos['tarifa']
-                     + experiencia_qualidade * cliente.pesos['qualidade']) / 10
-            medias.append(media)
+            avaliacao_cliente = (experiencia_anuidade * cliente.pesos['anuidade'] + experiencia_tarifa * cliente.pesos[
+                'tarifa'] + experiencia_qualidade * cliente.pesos['qualidade']) / 10
+            avaliacoes.append(avaliacao_cliente)
 
             valor = '\n' + str(cliente.id) + ',' + str(cliente.conta.saldo) + ',' + str(cliente.banco.id) + ',' + \
                     str(cliente.resiliencia_tarifa) + ',' + str("%.2f" % cliente.banco.tarifa) + ',' + \
                     str(cliente.verifica_tarifa()) + ',' + str(cliente.resiliencia_anuidade) + ',' + \
                     str("%.2f" % cliente.banco.anuidade) + ',' + str(cliente.verifica_anuidade()) + ',' + \
-                    str(cliente.resiliencia_qualidade) + ',' + str(cliente.banco.qualidade) + ',' + \
-                    str(cliente.verifica_qualidade()) + ',' + str(media)
+                    str(cliente.resiliencia_qualidade) + ',' + str("%.2f" % cliente.banco.qualidade) + ',' + \
+                    str(cliente.verifica_qualidade()) + ',' + str(avaliacao_cliente)
 
             valores.append(valor)
 
             # Contagem de satisfação dos clientes
-            if media >= 7:
+            if avaliacao_cliente >= 7:
                 num_sete += 1
-                media_por_faixa[cliente.conta.saldo][7] += 1
-            elif media >= 3:
+                satisfacao_por_faixa[cliente.conta.saldo][7] += 1
+            elif avaliacao_cliente >= 3:
                 num_tres += 1
-                media_por_faixa[cliente.conta.saldo][3] += 1
-            elif media > 0:
-                media_por_faixa[cliente.conta.saldo][0] += 1
+                satisfacao_por_faixa[cliente.conta.saldo][3] += 1
+            elif avaliacao_cliente > 0:
+                satisfacao_por_faixa[cliente.conta.saldo][0] += 1
                 troca = cliente.escolhe_banco(self.bancos)
                 num_trocaram_banco += troca
-                media_por_faixa[cliente.conta.saldo]['troca'] += troca
+                satisfacao_por_faixa[cliente.conta.saldo]['troca'] += troca
                 num_zero += 1
 
             # print("Cliente: ", cliente.id)
@@ -121,22 +123,22 @@ class Simulacao():
 
         satisfacao_por_renda = ['faixa de renda,satisfacao maior que 7, satisfacao menor que 7 e maior que 3, '
                                 'satisfacao menor que 3 e maior que 0, clientes que trocaram de banco']
-        for chave in media_por_faixa:
-            satisfacao = "\n" + str(chave) + "," + str(media_por_faixa[chave][7]) + "," \
-                         + str(media_por_faixa[chave][3]) + "," + str(media_por_faixa[chave][0]) + ',' \
-                         + str(media_por_faixa[chave]["troca"])
+        for chave in satisfacao_por_faixa:
+            satisfacao = "\n" + str(chave) + "," + str(satisfacao_por_faixa[chave][7]) + "," \
+                         + str(satisfacao_por_faixa[chave][3]) + "," + str(satisfacao_por_faixa[chave][0]) + ',' \
+                         + str(satisfacao_por_faixa[chave]["troca"])
             satisfacao_por_renda.append(satisfacao)
             print("Faixa de renda de ", chave)
-            print("Satisfacao maior que 7: ", media_por_faixa[chave][7])
-            print("Satisfacao maior que 3 e menor que 7: ", media_por_faixa[chave][3])
-            print("Satisfacao maior que 0 e menor que 3: ", media_por_faixa[chave][0])
+            print("Satisfacao maior que 7: ", satisfacao_por_faixa[chave][7])
+            print("Satisfacao maior que 3 e menor que 7: ", satisfacao_por_faixa[chave][3])
+            print("Satisfacao maior que 0 e menor que 3: ", satisfacao_por_faixa[chave][0])
 
             print("--------------------------------------------------")
 
-        media_final = sum(medias)/num_clientes
-        media_final = float("%.2f" % media_final)
+        media_final_satisfacao = sum(avaliacoes) / num_clientes
+        media_final_satisfacao = float("%.2f" % media_final_satisfacao)
         print("\nResultados consolidados dessa simulação: \n")
-        print("Média de satisfação: ", media_final)
+        print("Média de satisfação: ", media_final_satisfacao)
         print("Satisfação maior que 7: ", num_sete)
         print("Satisfação maior que 3 e menor que 7: ", num_tres)
         print("Satisfação maior que 0 e menor que 3: ", num_zero)
@@ -154,7 +156,7 @@ def guardar(arquivo, valores):
 if __name__ == '__main__':
     numero_repeticoes = int(input("Número de vezes para executar simulacao"))
     for i in range(numero_repeticoes):
-        print(f"{i+1}° simulação")
+        print(f"{i + 1}° simulação")
         simulacao = Simulacao()
         simulacao.criar_agentes()
         simulacao.executar_modelo()
